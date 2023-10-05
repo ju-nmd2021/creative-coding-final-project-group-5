4b28ef024cbf745bac1c6f10152348c3705f198d
Emil Karlsson <emil.h.karlsson@gmail.com>
2023-10-05 16:30:18 +0200

    i dont fucking know anymore.

--- wow.js
+++ wow.js
@@ -1,17 +1,17 @@
 let mic;
 let analyser;
+let fft;
+let frequencyText;
 
-window.addEventListener("load", () =>{
-    analyser = new Tone.Analyser("fft", 4096);
+window.addEventListener("load", () => {
+    fft = new Tone.FFT(2048);
+    analyser = new Tone.Analyser("fft", 2048);
     mic = new Tone.UserMedia();
     mic.connect(analyser);
     mic.open();
+    mic.toDestination();
 })
-/*
-window.addEventListener("click", () =>{
-    mic.open();
-})
-*/
+
 function setup() {
     createCanvas(500, 300);
     background(100);
@@ -25,5 +25,16 @@ function draw() {
     for (let i = 0; i < value.length; i++) {
         let v = map(value[i], -100, 0, height, 0);
         rect(i * 1, 0, 1, v); // waveform: * 100
+        if (v > 285) {
+            frequencyText = 'Frequency: ' + log(floor(fft.getFrequencyOfIndex(i))/440)/log(2) + 'Hz';
+            console.log(frequencyText);
+        }
     }
+    fill(0);
+    drawFrequencyText();
+    loop();
+}
+
+function drawFrequencyText(){
+    text(frequencyText,20,20)
 }
